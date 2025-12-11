ebd51505728fc809b6344be09c2a2e57db0df05a
Jakub <kubarafalski240703@gmail.com>
2025-12-11 23:37:41 +0100

    update 11.12

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,12 @@ import { Informations } from './informations/informations';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: Informations }
+    { path: 'home', component: Informations },
+    { path: 'about-us', component: Informations },
+    { path: 'apartaments', component: Informations },
+    { path: 'gallery', component: Informations },
+    { path: 'discount', component: Informations },
+    { path: 'opinions', component: Informations },
+    { path: 'contact', component: Informations },
+
 ];
