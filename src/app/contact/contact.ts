a34575f6cbadef8bf42beffed7b2c8f0c1124a9d
Jakub <kubarafalski240703@gmail.com>
2025-12-18 22:30:39 +0100

    added reactive form

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
 export class Contact {
-
+  contactForm = new FormGroup({
+    name: new FormControl<string>('', [Validators.required]),
+    email: new FormControl<string>('', [Validators.required]),
+    number: new FormControl<number | null>(null, [Validators.required]),
+    subject: new FormControl<string>('', [Validators.required]),
+    message: new FormControl<string>('', [Validators.required]),
+    check: new FormControl<boolean>(false, Validators.requiredTrue),
+  })
+  onSubmit() {
+    console.log(this.contactForm.value);
+    this.contactForm.reset();
+  }
 }
