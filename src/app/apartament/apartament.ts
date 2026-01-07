cd022dcbfc5c0bf27e298edc72eb46e73e7ac187
Jakub <kubarafalski240703@gmail.com>
2026-01-07 17:18:51 +0100

    update 07.01

--- src/app/apartament/apartament.ts
+++ src/app/apartament/apartament.ts
@@ -2,10 +2,19 @@ import { Component, inject, input, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApartamentsStorage } from '../apartaments-storage';
 import { ApartamentsModel } from '../apartaments.model';
+import { Field, form } from '@angular/forms/signals';
+type RegisterModel = {
+  message: string,
+  name: string,
+  email: string,
+  phoneNumber: number,
+  news: boolean,
+  loan: boolean
+}
 
 @Component({
   selector: 'app-apartament',
-  imports: [],
+  imports: [Field],
   templateUrl: './apartament.html',
   styleUrl: './apartament.css',
 })
@@ -13,8 +22,31 @@ export class Apartament implements OnInit {
   route: ActivatedRoute = inject(ActivatedRoute);
   apartamentsStorage = inject(ApartamentsStorage);
   apartament = signal<ApartamentsModel | undefined>(undefined);
+  
+  // permisions = [
+  //   {id: 1, title: 'news'},
+  //   {id: 2, title: 'loan'},
+  // ]
+
   ngOnInit(): void {
     const id = signal(this.route.snapshot.params['id']);
     this.apartament.set(this.apartamentsStorage.getApartamentById(id()));
   }
+
+  onSubmit(event: Event): void{
+    event.preventDefault();
+    console.log(this.signalRegisterModel())
+  }
+
+  signalRegisterModel = signal<RegisterModel>({
+    message: '',
+    name: '',
+    email: '',
+    phoneNumber: 123456789,
+    news: false,
+    loan: false,
+  })
+  // add validation
+  signalRegisterForm = form(this.signalRegisterModel);
+
 }
