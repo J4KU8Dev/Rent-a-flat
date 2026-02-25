b80db95b4f704ef789679f9f19955dc437b244bf
Jakub <kubarafalski240703@gmail.com>
2026-02-25 22:10:27 +0100

    customizing and adding open/close modal

--- src/app/auth/account/account.ts
+++ src/app/auth/account/account.ts
@@ -1,19 +1,31 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { AuthService } from '../../services/auth-service';
 import { LoginModel } from '../../login-model';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-account',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './account.html',
   styleUrl: './account.css',
 })
 export class Account implements OnInit{
   authService = inject(AuthService);
   user = signal<LoginModel | null>(null);
-  
+  clicked = signal<boolean>(false);
   ngOnInit(): void {
+    if(this.user==null){
+      // route to login component to prevent from checking account settins without logged account
+    }
     this.user.set(this.authService.currentUser());
+  }
+
+  onOpenModal() {
+    this.clicked.set(true);
+  }
+
+  onCloseModal() {
+    this.clicked.set(false);
   }
 
 }
